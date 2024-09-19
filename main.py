647f302134e76fff2576b7d09f0eb0eff95d4740
BryanChappellCode <bryanchappellcode@gmail.com>
2024-09-19 16:44:40 -0500

    added player object, updated constants and main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import SCREEN_HEIGHT,SCREEN_WIDTH
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -11,13 +12,18 @@ def main():
     fps = pygame.time.Clock()
     dt = 0
 
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     while(True):
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill(color="Black")
         pygame.display.flip()
+
+        player.draw(screen)
 
         dt = fps.tick(60) / 1000
 
